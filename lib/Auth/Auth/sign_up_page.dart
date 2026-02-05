43dc4dee711398e4ded683ce09b7b68e01e02fdf
Sagar Shrestha <sagarshresta200@gmail.com>
2026-02-05 23:00:08 +0545

    Feat:Signup Page UI designing

--- lib/Auth/Auth/sign_up_page.dart
+++ lib/Auth/Auth/sign_up_page.dart
@@ -6,18 +6,130 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                height: 100,
-                color: Colors.blue,
+      body: SafeArea(child: 
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 85,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 122-24*2),
+              child: Container(
+                child: Image.asset("assets/images/LOGO.png"),
               ),
-            ],
-          )
-        ],
-      ),
+            ),
+            
+            SizedBox(height: 60,),
+
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 34-24),
+                  child: Text("Getting Started.!",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 10,),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 35-24, right: 81-24),
+              child: Column(
+                children: [
+                  Text("Create an Account to Continue your allCourses",
+                  style: TextStyle(
+                  fontSize: 14,
+                  fontWeight:FontWeight.w700,
+                  ),),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 50,),
+
+            TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email_outlined),
+                hintText: "Email",
+                border: OutlineInputBorder()
+              ),
+            ),
+
+            SizedBox(height: 20,),
+
+            TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock),
+                hintText: "Password",
+                suffixIcon: Icon(Icons.visibility_off_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 24,),
+
+            Row(
+              children: [
+                Stack(
+                  children: [
+                   Column(mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                       Container(height: 24, width: 24,
+                        decoration:BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(100),
+                        ) ,
+                        child: Icon(Icons.check_circle, size: 16.1,color: Colors.white,),
+                         ),
+                         
+                     ],
+                   ),
+                  
+                  ]
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Text("Agree to Terms & Conditions",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800,
+                  ),),
+                ),
+
+                
+              ],
+            ),
+
+            SizedBox(height: 39,),
+
+                ClipRRect(
+                   borderRadius: BorderRadius.circular(50),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 17),
+                        child: Text("Sign Up",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                        ),),
+                        
+                      ),
+                      Spacer(),
+                      Icon(Icons.arrow_forward),
+                    ],
+                  ),
+                ),
+
+
+          ],
+        ),
+      )),
     );
   }
 }
